10403fe321f482654f3cd79a598bbd644d8449c5
Wojciech Pajtel <wojciech.pajtel@student.pk.edu.pl>
2025-05-30 13:13:07 +0200

    remove unused types

--- frontend/src/types/APIResponse.tsx
+++ frontend/src/types/APIResponse.tsx
@@ -9,10 +9,4 @@ export interface UserData {
     roles: string[];
 }
 
-export interface CSRFToken {
-    csrf: string;
-}
-
-export type UserResponse = APIResponse<UserData>;
-
 export type AuthType = "jwt" | "session" | "oauth" | null;
